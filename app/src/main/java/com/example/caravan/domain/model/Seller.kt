4f4c080115604120606a5b38f03da497576a08d1
CherifiMi <cherifi154949@gmail.com>
2022-07-25 04:48:04 +0100

    added seller

---
+++ app/src/main/java/com/example/caravan/domain/model/Seller.kt
@@ -0,0 +1,25 @@
+package com.example.caravan.domain.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Seller(
+    @SerializedName("autheId")
+    val autheId: String,
+    @SerializedName("brand")
+    val brand: String,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("isActive")
+    val isActive: Boolean,
+    @SerializedName("ordersId")
+    val ordersId: List<String>,
+    @SerializedName("owner")
+    val owner: String,
+    @SerializedName("phone")
+    val phone: String,
+    @SerializedName("productsId")
+    val productsId: List<String>,
+    @SerializedName("type")
+    val type: String
+)
