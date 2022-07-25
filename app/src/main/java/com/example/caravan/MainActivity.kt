a3f3e42f1a4313d51cbdd423608799588bda8b4a
CherifiMi <cherifi154949@gmail.com>
2022-07-25 04:47:28 +0100

    added splash screen

--- app/src/main/java/com/example/caravan/MainActivity.kt
+++ app/src/main/java/com/example/caravan/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.caravan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,34 +11,32 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.caravan.screens.MainViewModel
+import com.example.caravan.screens.Test
 import com.example.caravan.ui.theme.CaravanTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {setKeepOnScreenCondition{viewModel.spalsh.value}}
+
         setContent {
             CaravanTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                ){
+                    Test()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CaravanTheme {
-        Greeting("Android")
-    }
-}
