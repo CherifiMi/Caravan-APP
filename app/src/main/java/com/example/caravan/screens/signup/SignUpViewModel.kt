9cf3ff5d1e3a8e9e5a86134a99ac2ad81b04e487
CherifiMi <cherifi154949@gmail.com>
2022-07-26 04:22:41 +0100

    added states

--- app/src/main/java/com/example/caravan/screens/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/caravan/screens/signup/SignUpViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.caravan.screens.signup
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
@@ -8,4 +9,15 @@ class SignUpViewModel @Inject constructor(): ViewModel(){
 
     var selected_type = mutableStateOf(1)
 
+    var selectedText = mutableStateOf("")
+    var expanded = mutableStateOf(false)
+
+    var gmail = mutableStateOf("")
+    var password = mutableStateOf("")
+
+    var first_name = mutableStateOf("")
+    var last_name = mutableStateOf("")
+    var brand_name = mutableStateOf("")
+    var address = mutableStateOf("")
+    var phone = mutableStateOf("")
 }
