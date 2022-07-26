78356d55ac102471b4eeb55af2649390d2823044
CherifiMi <cherifi154949@gmail.com>
2022-07-26 13:05:18 +0100

    AccountService.kt interface

---
+++ app/src/main/java/com/example/caravan/domain/util/AccountService.kt
@@ -0,0 +1,7 @@
+package com.example.caravan.domain.util
+
+interface AccountService {
+    fun createAnonymousAccount(onResult: (Throwable?) -> Unit)
+    fun authenticate(email: String, password: String, onResult: (Throwable?) -> Unit)
+    fun createAccount(email: String, password: String, onResult: (Throwable?) -> Unit)
+}
