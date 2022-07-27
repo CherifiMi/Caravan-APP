f6408f2754d7fe5b8d432bca7c0038ad5831a38d
CherifiMi <cherifi154949@gmail.com>
2022-07-27 05:39:44 +0100

    screens routes

--- app/src/main/java/com/example/caravan/data/util/Constants.kt
+++ app/src/main/java/com/example/caravan/data/util/Constants.kt
@@ -3,5 +3,8 @@ package com.example.caravan.data.util
 class Constants {
     companion object{
         const val BASE_URL = "https://caravan-ap1.herokuapp.com/v1/"
+
+        const val MIN_PASS_LENGTH = 6
+        const val PASS_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$"
     }
 }
