277c9d35efe2b8c848fe72f7e17ddc71d29881eb
CherifiMi <cherifi154949@gmail.com>
2022-07-31 02:08:00 +0100

    navigation new screens

--- app/src/main/java/com/example/caravan/domain/navigation/Navigation.kt
+++ app/src/main/java/com/example/caravan/domain/navigation/Navigation.kt
@@ -5,17 +5,19 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.constraintlayout.compose.ExperimentalMotionApi
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.caravan.MainApp
 import com.example.caravan.MainViewModel
-import com.example.caravan.domain.navigation.Screens
 import com.example.caravan.ui.buyer.BuyerHomeScreen
 import com.example.caravan.ui.errors.NoNetScreen
 import com.example.caravan.ui.errors.SomethingWrongScreen
 import com.example.caravan.ui.login.LoginScreen
 import com.example.caravan.ui.rep.RepHomeScreen
 import com.example.caravan.ui.seller.SellerHomeScreen
+import com.example.caravan.ui.seller.components.SellerEditProduct
 import com.example.caravan.ui.signup.screens.*
 
 
@@ -26,12 +28,12 @@ import com.example.caravan.ui.signup.screens.*
 fun Navigation(
     navController: NavHostController,
     viewModel: MainViewModel,
-){
+) {
 
     NavHost(
         navController,
         startDestination =
-        when(viewModel.firstScreen){
+        when (viewModel.firstScreen) {
             "login" -> Screens.Login.route
             "wait" -> Screens.Wait.route
             "buyer" -> Screens.HomeBuyer.route
@@ -40,7 +42,7 @@ fun Navigation(
             "nonet" -> Screens.NoNetError.route
             else -> Screens.SWError.route
         }
-    ){
+    ) {
 
         composable(route = Screens.Main.route) {
             MainApp(viewModel)
@@ -70,9 +72,20 @@ fun Navigation(
         composable(route = Screens.HomeBuyer.route) {
             BuyerHomeScreen(navController = navController)
         }
+
+
         composable(route = Screens.HomeSeller.route) {
             SellerHomeScreen(navController = navController)
         }
+        composable(
+            route = Screens.ProductSeller.route,
+            arguments = listOf(navArgument(name = "item"){type = NavType.StringType})
+        ) {
+            var item = it.arguments
+            SellerEditProduct(navController = navController, args = item)
+        }
+
+
         composable(route = Screens.HomeRep.route) {
             RepHomeScreen(navController = navController)
         }
@@ -86,4 +99,6 @@ fun Navigation(
     }
 
 }
+
+
 
--- app/src/main/java/com/example/caravan/domain/navigation/Screens.kt
+++ app/src/main/java/com/example/caravan/domain/navigation/Screens.kt
@@ -11,8 +11,19 @@ sealed class Screens(val route: String) {
     object Wait: Screens(route = "wait_screen")
 
     object HomeBuyer: Screens(route = "home_buyer_screen")
+
+
     object HomeSeller: Screens(route = "home_seller_screen")
+    object ProductSeller: Screens(route = "product_seller_screen/{item}"){
+        fun passItem(item: String): String{
+            return "product_seller_screen/$item"
+        }
+    }
+
+
     object HomeRep: Screens(route = "home_rep_screen")
+
+
 
     object NoNetError: Screens(route = "no_net_screen")
     object SWError: Screens(route = "all_error_screen")
