5a2f555d7f52da0f7ad97853b8b97c40f4f31c87
CherifiMi <cherifi154949@gmail.com>
2022-08-02 01:04:36 +0100

    made card pretty

--- app/src/main/java/com/example/caravan/common/components/MyProductItem.kt
+++ app/src/main/java/com/example/caravan/common/components/MyProductItem.kt
@@ -5,38 +5,124 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.caravan.R
 import com.example.caravan.domain.model.Product
+import com.example.caravan.theme.*
 
 @Composable
-fun MyProductItem(item: Product, function: () -> Unit) {
+fun MyProductItem(item: Product, cart: Boolean = false, function: () -> Unit) {
     Card(
         shape = RoundedCornerShape(15.dp),
         elevation = 2.dp,
         modifier = Modifier
             .padding(8.dp)
-            .clickable { function() }
+
     ) {
-        Column() {
-            AsyncImage(
-                modifier = Modifier
-                    .height(125.dp),
-                model = item.imageUrls[0],
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
-            Text(text = item.name)
-            Text(text = item.newPrice.toString())
-            Text(text = item.initPrice.toString())
-            Text(text = item.minOrder)
-            Spacer(modifier = Modifier.padding(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { function() },
+            contentAlignment = Alignment.TopStart
+        ) {
+
+            Column() {
+                AsyncImage(
+                    modifier = Modifier
+                        .height(125.dp),
+                    model = item.imageUrls[0],
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = item.name + "Lorem Ipsum, giving information on its origins, as well as a random Lipsum",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = TextStyle(
+                        fontFamily = Montserrat,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 13.sp
+                    ), modifier = Modifier.padding(8.dp)
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp),
+                ) {
+                    Column() {
+                        Text(
+                            text = "SR " + item.newPrice.toString() + ",00",
+                            style =
+                            TextStyle(
+                                fontFamily = Montserrat,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 13.sp
+                            ),
+                        )
+                        Text(
+                            text = "SR " + item.initPrice.toString() + ",00",
+                            style =
+                            TextStyle(
+                                fontFamily = Montserrat,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 10.sp
+                            ),
+                            textDecoration = TextDecoration.LineThrough
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            modifier = Modifier.alpha(if(cart) 1f else 0f).padding(12.dp),
+                            painter = painterResource(id = R.drawable.cart_full),
+                            tint = LightGrey,
+                            contentDescription = null
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+            Box(
+                Modifier
+                    .padding(8.dp)
+                    .wrapContentWidth()
+                    .background(shape = RoundedCornerShape(5.dp), color = PinkRed)
+            ) {
+                val disP = 100 - ((item.newPrice.toFloat() / item.initPrice.toFloat()) * 100)
+                Text(
+                    text = "${disP.toInt()}%",
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+                    style =
+                    TextStyle(
+                        color = Color.White,
+                        fontFamily = Montserrat,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 11.sp
+                    )
+                )
+            }
         }
+
+
     }
 
 }
