aab37a786838da358aa33af2a4bb8d9b7cd591c3
CherifiMi <cherifi154949@gmail.com>
2022-08-02 01:05:49 +0100

    init buyer details

--- app/src/main/java/com/example/caravan/ui/buyer/BuyerViewModel.kt
+++ app/src/main/java/com/example/caravan/ui/buyer/BuyerViewModel.kt
@@ -3,9 +3,12 @@ package com.example.caravan.ui.buyer
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
+import com.example.caravan.data.repository.AccountService
 import com.example.caravan.data.repository.CaravanRepository
 import com.example.caravan.data.util.Result
 import com.example.caravan.domain.model.ProductsList
+import com.example.caravan.domain.navigation.Screens
 import com.example.caravan.domain.use_cases.GetProductsUseCase
 import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,14 +23,26 @@ import javax.inject.Inject
 @HiltViewModel
 class BuyerViewModel @Inject constructor(
     private val getProductsUseCase: GetProductsUseCase,
-    private val repository: CaravanRepository
+    private val repository: CaravanRepository,
+    private val accountService: AccountService
 ) : ViewModel() {
+
+    fun signOut(navController: NavHostController){
+
+        accountService.signOut()
+
+        navController.navigate(Screens.Main.route) {
+            launchSingleTop = true
+            popUpTo(0) { inclusive = true }
+        }
+    }
 
     val savedData =
         Gson().fromJson(
             runBlocking (Dispatchers.IO){
                 repository.getSavedProductList()
             }.productList, ProductsList::class.java)
+
 
     fun getProducts() {
         Log.d("TESTAPI", "loading")
